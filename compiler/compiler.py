114653b8ca6bde0c47905d59eba6efc4234ccbb1
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-10-03 12:26:28 -0700

    added pytests

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,8 +1,7 @@
 import sys
 import rich
 
-import stringHelp
-import helper
+import stringHelp, helper
 
 current_line = 0
 variables = {}
@@ -11,9 +10,9 @@ def top_level(line):
     if line.startswith("#"):
         return
     elif line.startswith("say"):
-        say(line)
+        return say(line)
     elif stringHelp.count("=", line) == 1 and stringHelp.position("=", line)[0] != 0:
-        set_variable(line)
+        return set_variable(line)
 
 
 def say(line):
@@ -28,11 +27,15 @@ def say(line):
         line = line.lstrip("say")
         line = line.replace(" ", "")
         line = line.split(',')
+        full_out = ""
         for i in line:
-            try: print(variables[i], end = " ")
+            try: 
+                print(variables[i], end = " ")
+                full_out += str(variables[i]) + " "
             except KeyError: raise Exception("Variable not found")
         print("\n", end = "")
-        return
+        full_out += "\n"
+        return full_out
 
     if stringHelp.count("'", line) == 0 and stringHelp.count('"', line) == 0:
         line = line.rstrip("\n")
@@ -43,21 +46,21 @@ def say(line):
         return line
 
     #kavish's code goes here
-    
-    
-    if listed[3] == " " and listed[4] in quotes:
-        start = 5
-        quote_used = listed[4]
-    elif listed[3] in quotes:
-        quote_used = listed[3]
-        start = 4        
-    if start == None:
-        raise Exception(f"Error on line: {line}\nInvalid syntax for say statement. Did you add too many spaces or forget quotes?")
-    for i in listed[start:]:
-        if i in quote_used:
-            break
-        out += i
-    print(out)
+    else:
+        if listed[3] == " " and listed[4] in quotes:
+            start = 5
+            quote_used = listed[4]
+        elif listed[3] in quotes:
+            quote_used = listed[3]
+            start = 4        
+        if start == None:
+            raise Exception(f"Error on line: {line}\nInvalid syntax for say statement. Did you add too many spaces or forget quotes?")
+        for i in listed[start:]:
+            if i in quote_used:
+                break
+            out += i
+        print(out)
+        return out
 
 
 def set_variable(line):
@@ -77,21 +80,28 @@ def set_variable(line):
     value = value.lstrip((" "))
     value = helper.convert(value,helper.datatype(value))
     variables[name] = value
+    return variables[name]
 
-        
-
-if __name__ == "__main__":
+def main(filename):
+    global current_line
     #print the intro
     rich.print("[yellow]Hello From The Alter Community[/yellow]")
     #load file and scan for errors, print out a custom message if there were errors
     lines = []
-    with open(sys.argv[1], "r") as file:
+    with open(filename, "r") as file:
         raw = file.readlines()
         for i in raw:
             lines.append(i)
             current_line += 1
+    out = []
     for i in lines:
-        top_level(i)
+        line_out = top_level(i)
+        if line_out != None:
+            out.append(line_out)
     print(lines, variables, sep="\n")
     rich.print("[bold green]No Errors![/bold green]")
     rich.print("[bold blue]Program exited with code 0[/bold blue]")
+    return out
+
+if __name__ == "__main__":
+    main(sys.argv[1])
